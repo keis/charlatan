342f1be9ef94619768952db2dde758e33b71df02
David Keijser <david.keijser@klarna.com>
2014-05-22 15:58:05 +0200

    naive capability to send in msg app

--- src/msg.c
+++ src/msg.c
@@ -12,11 +12,16 @@ GMainLoop *loop;
 static char verbose;
 static char acknowledge;
 static char list_messages;
+static char send_message;
+static char single_target;
 static char **users;
 
 struct MessageWriter {
     char first;
 } writer = {1};
+
+#define BUF_SIZE 1024
+static char msg_buffer[BUF_SIZE];
 
 /* commandline arguments */
 const
@@ -27,10 +32,24 @@ GOptionEntry entries[] = {
         "Acknowledge pending messages.", NULL },
     { "who", 'w', 0, G_OPTION_ARG_NONE, &list_messages,
         "List open channels", NULL },
+    { "send", 's', 0, G_OPTION_ARG_NONE, &send_message,
+        "Send a message", NULL },
     { G_OPTION_REMAINING, 0, 0, G_OPTION_ARG_STRING_ARRAY, &users,
         "USER", NULL },
     { NULL, 0, 0, 0, NULL, NULL, NULL }
 };
+
+static gboolean
+contact_status_unknown (TpContact *contact)
+{
+    return strcmp (tp_contact_get_presence_status (contact), "unknown") != 0;
+}
+
+static gboolean
+single_element (char **array)
+{
+    return array && array[0] && !array[1];
+}
 
 static void
 message_cb (TpMessage *message)
@@ -73,9 +92,17 @@ contact_cb (ChVisitor *visitor,
 {
     (void) visitor;
 
-    printf ("%s\t%s\n",
-            tp_contact_get_alias (contact),
-            tp_contact_get_identifier (contact));
+    if (list_messages) {
+        printf ("%s\t%s\n",
+                tp_contact_get_alias (contact),
+                tp_contact_get_identifier (contact));
+    }
+
+    if (send_message) {
+        if (contact_status_unknown (contact)) {
+            ch_visitor_visit_contact_channel (visitor, contact);
+        }
+    }
 }
 
 static void
@@ -91,12 +118,35 @@ ack_messages_cb (GObject      *source,
                                          &error);
 }
 
+void
+send_message_cb (GObject      *source,
+                 GAsyncResult *result,
+                 gpointer      user_data)
+{
+    ChVisitor *visitor = (ChVisitor*) user_data;
+    gchar *token;
+    GError *error;
+
+    if (!tp_text_channel_send_message_finish (source, result, &token, &error)) {
+        g_printerr ("error: %s\n", error->message);
+        return;
+    }
+
+    if (verbose) {
+        g_printerr ("message sent %s", token);
+    }
+
+    ch_visitor_decref (visitor);
+}
+
 static void
 channel_ready (GObject      *source,
                GAsyncResult *result,
                gpointer      user_data)
 {
     ChVisitor *visitor = (ChVisitor*) user_data;
+    TpChannel *channel;
+    TpMessage *message;
 
     GError *error = NULL;
     if (!tp_proxy_prepare_finish (source, result, &error)) {
@@ -104,7 +154,7 @@ channel_ready (GObject      *source,
         return;
     }
 
-    TpChannel *channel = TP_CHANNEL (source);
+    channel = TP_CHANNEL (source);
 
     if (verbose > 0) {
         g_printerr ("channel_ready \"%s\" (type %s)\n",
@@ -130,6 +180,16 @@ channel_ready (GObject      *source,
         }
 
         g_list_free_full (messages, g_object_unref);
+
+        if (send_message) {
+            message = tp_client_message_new_text(0, msg_buffer);
+            ch_visitor_incref (visitor);
+            tp_text_channel_send_message_async (TP_TEXT_CHANNEL (channel),
+                                                message,
+                                                0,
+                                                send_message_cb,
+                                                visitor);
+        }
     } else {
         g_printerr ("error: %s is not a text channel\n",
                     tp_channel_get_identifier (channel));
@@ -186,6 +246,7 @@ connection_cb (ChVisitor    *visitor,
                guint         status)
 {
     (void) visitor;
+    char **userit;
 
     TpContact *self;
     if (status == 0) {
@@ -198,7 +259,15 @@ connection_cb (ChVisitor    *visitor,
                         self ? tp_contact_get_identifier (self) : "n/a");
         }
 
-        ch_visitor_visit_channels (visitor, connection);
+        if (send_message) {
+            for (userit = users; *userit; userit += 1) {
+                ch_visitor_visit_connection_contact (visitor,
+                                                     connection,
+                                                     *userit);
+            }
+        } else {
+            ch_visitor_visit_channels (visitor, connection);
+        }
     }
 }
 
@@ -208,6 +277,12 @@ dispose_cb (ChVisitor *visitor)
     (void) visitor;
 
     g_main_loop_quit (loop);
+}
+
+static void
+read_message ()
+{
+    fread (msg_buffer, BUF_SIZE, 1, stdin);
 }
 
 int
@@ -225,6 +300,16 @@ main (int argc, char **argv)
     g_option_context_add_main_entries (context, entries, NULL);
     g_option_context_parse (context, &argc, &argv, NULL);
     g_option_context_free (context);
+
+    single_target = single_element (users);
+
+    if (single_target) {
+        send_message = TRUE;
+    }
+
+    if (send_message) {
+        read_message ();
+    }
 
     loop = g_main_loop_new (NULL, FALSE);
 
